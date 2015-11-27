5c568b75e1347ea505c1bef709e52415186a5ed3
Shafeeq Rabbani <rabbass@mcmaster.ca>
2015-11-27 09:35:29 -0500

    Bugs in Food fixed. Snake in working condition

--- Source/Food.py
+++ Source/Food.py
@@ -1,7 +1,6 @@
 import pygame
 import random
 
-
 class Food:
     """
     Food class to encapsulate position of food particle
@@ -18,24 +17,25 @@ class Food:
         """
 
         #width of block
-        block_width = 10
+        blockWidth = 10
 
         #length of block
-        block_length = 10
+        blockLength = 10
 
         #scale factor to multiply by the randomly generated number to appear on screen.
-        scale_factor = 10
+        scaleFactor = 10
 
+     
         #offset to skip the header (which contains score)
         offset = 20
 
         #randomly generate the a y position on the screen to display the food
-        x_val = random.randrange(55)
+        yVal = random.randrange(55)
 
         #randomly generate the an x position on the screen to display the food
-        y_val = random.randrange(48)
+        xVal = random.randrange(48)
 
         #position has x and y values.
-        self.position = pygame.Rect(x_val * scale_factor, y_val * scale_factor + offset, block_width, block_length)
+        self.position = pygame.Rect(xVal * scaleFactor, yVal * scaleFactor + offset, blockWidth, blockLength)
 
 
