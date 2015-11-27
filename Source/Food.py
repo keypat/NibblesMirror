db844f06a643183238110927123a3c7bcaf88be4
Shafeeq Rabbani <rabbass@mcmaster.ca>
2015-11-27 09:44:38 -0500

    xVal and yVal swapped in Food.py

--- Source/Food.py
+++ Source/Food.py
@@ -29,11 +29,11 @@ class Food:
         #offset to skip the header (which contains score)
         offset = 20
 
-        #randomly generate the a y position on the screen to display the food
-        yVal = random.randrange(55)
+        #randomly generate the a x position on the screen to display the food
+        xVal = random.randrange(55)
 
-        #randomly generate the an x position on the screen to display the food
-        xVal = random.randrange(48)
+        #randomly generate the an y position on the screen to display the food
+        yVal = random.randrange(48)
 
         #position has x and y values.
         self.position = pygame.Rect(xVal * scaleFactor, yVal * scaleFactor + offset, blockWidth, blockLength)
