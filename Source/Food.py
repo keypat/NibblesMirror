950ec2f9f50454dd998930e9dd5409a2f98f21c6
Keyur Patel <patelk36@mcmaster.ca>
2015-11-27 08:51:18 -0500

    fixed food.py

--- Source/Food.py
+++ Source/Food.py
@@ -18,22 +18,22 @@ class Food:
         """
 
         #width of block
-        int block_width = 10
+        block_width = 10
 
         #length of block
-        int block_length = 10
+        block_length = 10
 
         #scale factor to multiply by the randomly generated number to appear on screen.
-        int scale_factor = 10
+        scale_factor = 10
 
         #offset to skip the header (which contains score)
-        int offset = 20
+        offset = 20
 
         #randomly generate the a y position on the screen to display the food
-        int x_val = random.randrange(55)
+        x_val = random.randrange(55)
 
         #randomly generate the an x position on the screen to display the food
-        int y_val = random.randrange(48)
+        y_val = random.randrange(48)
 
         #position has x and y values.
         self.position = pygame.Rect(x_val * scale_factor, y_val * scale_factor + offset, block_width, block_length)
