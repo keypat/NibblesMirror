fd41b4f0c6d11acc8a2d18f994192c8433736f09
Keyur Patel <patelk36@mcmaster.ca>
2015-11-28 19:51:38 -0500

    updates

--- Source/PlayMap.py
+++ Source/PlayMap.py
@@ -31,7 +31,10 @@ class PlayMap:
                 output:none
                 """
                 
+<<<<<<< HEAD
                 
+=======
+>>>>>>> 73e749b709e6e2a4140f64775ea02dfa017fbde5
                 self.height = 500
                 self.width = 550
                 self.snake = Snake() # starting coord of snake
