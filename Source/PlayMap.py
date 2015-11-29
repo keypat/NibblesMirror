19c15ca5f1808ea994dbee326202890b6df2c007
Keyur Patel <patelk36@mcmaster.ca>
2015-11-28 19:54:40 -0500

    updates

--- Source/PlayMap.py
+++ Source/PlayMap.py
@@ -32,7 +32,10 @@ class PlayMap:
                 """
                 
 <<<<<<< HEAD
+<<<<<<< HEAD
                 
+=======
+>>>>>>> 73e749b709e6e2a4140f64775ea02dfa017fbde5
 =======
 >>>>>>> 73e749b709e6e2a4140f64775ea02dfa017fbde5
                 self.height = 500
